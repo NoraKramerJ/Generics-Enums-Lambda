7544aa7b6008b3fa33d1e1be3fae5242a2a2253e
Nmerkel <noramerkel0@gmail.com>
2022-08-23 20:14:53 -0400

    created a class to create the object and run the main method to test it

--- src/main/java/Generics/IntegerPrinter.java
+++ src/main/java/Generics/IntegerPrinter.java
@@ -1,4 +1,13 @@
 package Generics;
 
 public class IntegerPrinter {
+    Integer thingsToPrint;
+
+    public IntegerPrinter(Integer thingsToPrint) {
+        this.thingsToPrint = thingsToPrint;
+    }
+
+    public void print(){
+        System.out.println(thingsToPrint);
+    }
 }
