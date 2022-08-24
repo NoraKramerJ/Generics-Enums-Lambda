e0eb024a76a7a1ec3f0868bac9f5fdec29ce76a0
Nmerkel <noramerkel0@gmail.com>
2022-08-23 22:18:06 -0400

    Streams, using forEach, filter, and sorted

---
+++ src/main/java/javaStreams/Main.java
@@ -0,0 +1,25 @@
+package javaStreams;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+
+    List<Person> females=PersonData.getAll().
+            stream().filter(p->p.getGender()
+            .equals(Gender.FEMALE)).collect(Collectors.toList());
+
+   // females.forEach(System.out::println);
+   List<Person> sortedByAge= PersonData.getAll().stream().sorted(Comparator.comparing(Person::getAge)).collect(Collectors.toList());
+     sortedByAge.forEach(System.out::println);
+  }
+
+
+}
---
+++ src/main/java/javaStreams/PersonData.java
@@ -0,0 +1,20 @@
+package javaStreams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PersonData {
+
+    public static List<Person> getAll(){
+        return
+                Arrays.asList(new Person("Aseel", 30, Gender.FEMALE),
+        new Person("Alina Johnson", 31, Gender.FEMALE),
+                new Person("Mike Soyar", 35, Gender.MALE),
+                new Person("Lorraine", 25, Gender.FEMALE),
+                new Person("James Bond", 87, Gender.MALE),
+                new Person("Brooke Hudson", 29, Gender.FEMALE),
+                new Person("Zyraa", 24, Gender.FEMALE)
+        );
+    }
+}
